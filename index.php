843fb86cfcfe2578d6295994676a7aea864e7c58
Max <maxblanck@outlook.fr>
2020-12-08 11:40:50 +0100

    php Partie 2  Exercice 2

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Exercice 2 Partie 2</title>
+</head>
+<body>
+    <h1>Exercice 2 Partie 2</h1>
+
+    <?php
+    $isEasy = true;
+    if($isEasy)
+    {
+        echo "C'est Facile !!";
+    }
+    else 
+    {
+        echo "C'est difficile !!!";
+    }
+    ?>
+
+</body>
+</html>
